44650974c91a6e5053ad18ca1afd726e9bdf86a5
Christoph <christoph.tittel@gmail.com>
2019-11-23 21:49:38 +0100

    logo transparent, added logo to page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
 import React from 'react';
 
-export const Header = () => <div>HEADER</div>;
+export const Header = () => <img src={process.env.PUBLIC_URL + '/logo-name.png'} height="100"></img>;
+// {/* <div>HEADER</div>; */}
