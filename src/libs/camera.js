2d8c8616a398a6b5bb01780737f84e03c5f7f020
momenator <muhammad.rafdi@motius.de>
2019-11-24 09:24:29 +0100

    Changed default algorithm

--- src/libs/camera.js
+++ src/libs/camera.js
@@ -76,7 +76,7 @@ const defaultResNetStride = 32;
 const defaultResNetInputResolution = 250;
 
 const guiState = {
-  algorithm: 'multi-pose',
+  algorithm: 'single-pose',
   input: {
     architecture: 'MobileNetV1',
     outputStride: defaultMobileNetStride,
